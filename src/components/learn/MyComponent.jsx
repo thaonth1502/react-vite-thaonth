db4ac95429988c7ca45841bb05fd8257f3613224
thaonth1502 <huongthao89@gmail.com>
2026-03-12 10:11:09 +0700

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,14 @@
 // () => {}
 // component = html + css + js
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div> Rosemary & Huong Thao </div>
-
+        <>
+            <div> Rosemary & Huong Thao </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}> Child </div>
+        </>
     );
-
 }
-
 export default MyComponent;
