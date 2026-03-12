1b2440e98d5925cc2bca39fd285fc98ca1d9e28e
thaonth1502 <huongthao89@gmail.com>
2026-03-12 10:27:15 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,15 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const name = "Rosemary"; //string
+    const person = {
+        name: "Rosemary",
+        age: 37
+    }
     return (
         <>
-            <div> Rosemary & Huong Thao </div>
+            <div> {JSON.stringify(person)} - Huong Thao </div>
+            <div>{console.log("ROSEMARY")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}> Child </div>
         </>
