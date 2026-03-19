344c058c4dfd4cf9a3252856cc43cdee1b5f297e
thaonth1502 <huongthao89@gmail.com>
2026-03-19 09:24:54 +0700

    video 47

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>React Vite - @thaonth</p>
+        </div>
+    )
+}
+export default Footer;
