c36972ef0a4cb95d4652e2f6a4af4877504ebd4d
thaonth1502 <huongthao89@gmail.com>
2026-03-25 10:16:10 +0700

    video 73

--- src/main.jsx
+++ src/main.jsx
@@ -48,6 +48,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+    < RouterProvider router={router} />
   </React.StrictMode>,
+
 )
