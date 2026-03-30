f40babbe042201d6cc2d5e8f2303b6d16a6efb06
thaonth1502 <huongthao89@gmail.com>
2026-03-30 18:34:40 +0700

    video 112

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
 
 //Set config defaults when creating the instance
 const instance = axios.create({
@@ -8,12 +14,14 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+    NProgress.start();
     if (typeof window !== "undefined" && window && window.localStorage && window.localStorage.getItem('access_token')) {
         config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
     }
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done();
     // Do something with request error
     return Promise.reject(error);
 });
@@ -21,12 +29,14 @@ instance.interceptors.request.use(function (config) {
 // Add a response interceptor
 instance.interceptors.response.use(
     function (response) {
+        NProgress.done();
         // Any status code that lies within the range of 2xx causes this function to trigger
         // Do something with response data
         if (response.data && response.data.data) return response.data;
         return response;
     },
     function (error) {
+        NProgress.done();
         // Any status codes that fall outside the range of 2xx cause this function to trigger
         // Do something with response error
         if (error.response && error.response.data) return error.response.data;
